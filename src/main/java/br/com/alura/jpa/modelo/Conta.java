53f7947d84d1952a24d6f6e2834184f9b50035c5
Adriano Avelino <adriano-st777@hotmail.com>
2021-02-01 16:13:36 -0300

    adiciona exemplo de relacionamento biderecional

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -1,9 +1,12 @@
 package br.com.alura.jpa.modelo;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Conta {
@@ -14,6 +17,9 @@ public class Conta {
 	private Integer agencia;
 	private Integer numero;
 	private Double saldo;
+	
+	@OneToMany(mappedBy = "conta")
+	private List<Movimentacao> movimentacoes;
 
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
@@ -52,5 +58,9 @@ public class Conta {
 
 	public void setNumero(Integer numero) {
 		this.numero = numero;
+	}
+
+	public List<Movimentacao> getMovimentacoes() {
+		return this.movimentacoes;
 	}
 }
