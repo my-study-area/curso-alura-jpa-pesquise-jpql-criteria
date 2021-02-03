613837e6535d8fcf673568e7e12a919b87f9714a
Adriano Avelino <adriano-st777@hotmail.com>
2021-02-03 11:46:54 -0300

    atualiza JPQL usando group by projetando resultado em array

--- src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.Query;
 
 public class TesteMediaDiariaDasMovimentacoes {
 
@@ -13,13 +13,21 @@ public class TesteMediaDiariaDasMovimentacoes {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
 		EntityManager em = emf.createEntityManager();
+
+//		String jpqlMedia = "select avg(m.valor) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
+//		TypedQuery<Double> queryMedia = em.createQuery(jpqlMedia, Double.class);
+//		List<Double> mediaDoValorDasMovimentacoes = queryMedia.getResultList();
+//		
+//		for (Double media : mediaDoValorDasMovimentacoes) {
+//			System.out.println("A Média das movimentações é: "+ media);
+//		}
 		
-		String jpqlMedia = "select avg(m.valor) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
-		TypedQuery<Double> queryMedia = em.createQuery(jpqlMedia, Double.class);
-		List<Double> mediaDoValorDasMovimentacoes = queryMedia.getResultList();
+		String jpqlMedia = "select avg(m.valor), day(m.data), month(m.data) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
+		Query queryMedia = em.createQuery(jpqlMedia);
+		List<Object[]> mediasDosValoresDasMovimentacoes = queryMedia.getResultList();
 		
-		for (Double media : mediaDoValorDasMovimentacoes) {
-			System.out.println("A Média das movimentações é: "+ media);
+		for (Object[] resultado : mediasDosValoresDasMovimentacoes) {
+			System.out.println("A Média das movimentações do dia " + resultado[1] + "/" + resultado[2] + "  é: "+ resultado[0]);
 		}
 	}
 
