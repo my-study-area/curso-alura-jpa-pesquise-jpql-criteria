fbf11930025373b23717cca5a83712455337df6c
Adriano Avelino <adriano-st777@hotmail.com>
2021-02-04 12:06:55 -0300

    atualiza exemplos com organização do código

--- src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
@@ -2,19 +2,15 @@ package br.com.alura.jpa.testes;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-
 import br.com.alura.jpa.modelo.MediaComData;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
 
 public class TesteMediaDiariaDasMovimentacoes {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
-		EntityManager em = emf.createEntityManager();
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+//		EntityManager em = emf.createEntityManager();
 
 //		String jpqlMedia = "select avg(m.valor) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
 //		TypedQuery<Double> queryMedia = em.createQuery(jpqlMedia, Double.class);
@@ -32,13 +28,19 @@ public class TesteMediaDiariaDasMovimentacoes {
 //			System.out.println("A Média das movimentações do dia " + resultado[1] + "/" + resultado[2] + "  é: "+ resultado[0]);
 //		}
 		
-		String jpqlMedia = "select new br.com.alura.jpa.modelo.MediaComData(avg(m.valor), day(m.data), month(m.data)) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
-		TypedQuery<MediaComData> queryMedia = em.createQuery(jpqlMedia, MediaComData.class);
-		List<MediaComData> mediasDosValoresDasMovimentacoes = queryMedia.getResultList();
-
+//		String jpqlMedia = "select new br.com.alura.jpa.modelo.MediaComData(avg(m.valor), day(m.data), month(m.data)) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
+//		TypedQuery<MediaComData> queryMedia = em.createQuery(jpqlMedia, MediaComData.class);
+//		List<MediaComData> mediasDosValoresDasMovimentacoes = queryMedia.getResultList();
+//
+//		for (MediaComData resultado : mediasDosValoresDasMovimentacoes) {
+//			System.out.println("A Média das movimentações do dia " + resultado.getDia() + "/" + resultado.getMes() + "  é: "+ resultado.getValor());
+//		}
+		
+		List<MediaComData> mediasDosValoresDasMovimentacoes = new MovimentacaoDao().getMediaDiariaDasMovimentacoes();
 		for (MediaComData resultado : mediasDosValoresDasMovimentacoes) {
 			System.out.println("A Média das movimentações do dia " + resultado.getDia() + "/" + resultado.getMes() + "  é: "+ resultado.getValor());
 		}
+		
 	}
 
 }
--- src/main/java/br/com/alura/jpa/testes/TesteSomaEMediaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TesteSomaEMediaDasMovimentacoes.java
@@ -2,25 +2,27 @@ package br.com.alura.jpa.testes;
 
 import java.math.BigDecimal;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
 
 public class TesteSomaEMediaDasMovimentacoes {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
-		EntityManager em = emf.createEntityManager();
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+//		EntityManager em = emf.createEntityManager();
 		
-		String jpqlSoma = "select sum(m.valor) from Movimentacao m";
-		TypedQuery<BigDecimal> querySoma = em.createQuery(jpqlSoma, BigDecimal.class);
-		BigDecimal somaDoValorDasMovimentacoes = querySoma.getSingleResult();
+//		String jpqlSoma = "select sum(m.valor) from Movimentacao m";
+//		TypedQuery<BigDecimal> querySoma = em.createQuery(jpqlSoma, BigDecimal.class);
+//		BigDecimal somaDoValorDasMovimentacoes = querySoma.getSingleResult();
 		
-		String jpqlMedia = "select avg(m.valor) from Movimentacao m";
-		TypedQuery<Double> queryMedia = em.createQuery(jpqlMedia, Double.class);
-		Double mediaDoValorDasMovimentacoes = queryMedia.getSingleResult();
+		
+//		String jpqlMedia = "select avg(m.valor) from Movimentacao m";
+//		TypedQuery<Double> queryMedia = em.createQuery(jpqlMedia, Double.class);
+//		Double mediaDoValorDasMovimentacoes = queryMedia.getSingleResult();
+		
+		MovimentacaoDao dao = new MovimentacaoDao();
+		BigDecimal somaDoValorDasMovimentacoes = dao.getSomaDasMovimentacoes();
+		Double mediaDoValorDasMovimentacoes = dao.getMediaDasMovimentacoes();
 		
 		System.out.println("A soma das movimentações é: "+ somaDoValorDasMovimentacoes);
 		System.out.println("A Média das movimentações é: "+ mediaDoValorDasMovimentacoes);
