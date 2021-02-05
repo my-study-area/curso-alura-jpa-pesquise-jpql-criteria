bf94829814fd72da2cdbae458ebb6d7a191275a9
Adriano Avelino <adriano-st777@hotmail.com>
2021-02-05 12:04:14 -0300

    adiciona exemplo de CriteriaQuery com soma e média

--- src/main/java/br/com/alura/jpa/testes/TesteSomaEMediaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TesteSomaEMediaDasMovimentacoes.java
@@ -5,8 +5,13 @@ import java.math.BigDecimal;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
 
-import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 public class TesteSomaEMediaDasMovimentacoes {
 
@@ -24,11 +29,30 @@ public class TesteSomaEMediaDasMovimentacoes {
 //		TypedQuery<Double> queryMedia = em.createQuery(jpqlMedia, Double.class);
 //		Double mediaDoValorDasMovimentacoes = queryMedia.getSingleResult();
 		
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+//		EntityManager em = emf.createEntityManager();
+//		MovimentacaoDao dao = new MovimentacaoDao(em);
+//		BigDecimal somaDoValorDasMovimentacoes = dao.getSomaDasMovimentacoes();
+//		Double mediaDoValorDasMovimentacoes = dao.getMediaDasMovimentacoes();
+		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
 		EntityManager em = emf.createEntityManager();
-		MovimentacaoDao dao = new MovimentacaoDao(em);
-		BigDecimal somaDoValorDasMovimentacoes = dao.getSomaDasMovimentacoes();
-		Double mediaDoValorDasMovimentacoes = dao.getMediaDasMovimentacoes();
+		
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<BigDecimal> query = builder.createQuery(BigDecimal.class);
+		Root<Movimentacao> root = query.from(Movimentacao.class);
+		Expression<BigDecimal> sum = builder.sum(root.get("valor"));
+		query.select(sum);
+		TypedQuery<BigDecimal> typedQuery = em.createQuery(query);
+		BigDecimal somaDoValorDasMovimentacoes = typedQuery.getSingleResult();
+		
+		CriteriaBuilder builderMedia = em.getCriteriaBuilder();
+		CriteriaQuery<Double> queryMedia = builder.createQuery(Double.class);
+		Root<Movimentacao> rootMedia = queryMedia.from(Movimentacao.class);
+		Expression<Double> avg = builderMedia.avg(rootMedia.get("valor"));
+		queryMedia.select(avg);
+		TypedQuery<Double> typedQueryMedia = em.createQuery(queryMedia);
+		Double mediaDoValorDasMovimentacoes = typedQueryMedia.getSingleResult();
 		
 		System.out.println("A soma das movimentações é: "+ somaDoValorDasMovimentacoes);
 		System.out.println("A Média das movimentações é: "+ mediaDoValorDasMovimentacoes);
