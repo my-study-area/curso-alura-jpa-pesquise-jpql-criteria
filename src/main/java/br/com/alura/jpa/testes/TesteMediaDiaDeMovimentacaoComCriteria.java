d08cd6d73a2f628cc939f749f5d017c64951fdb8
Adriano Avelino <adriano-st777@hotmail.com>
2021-02-05 15:58:59 -0300

    adiciona teste de uso do método com CriteriaQuery para movimentações por data

---
+++ src/main/java/br/com/alura/jpa/testes/TesteMediaDiaDeMovimentacaoComCriteria.java
@@ -0,0 +1,29 @@
+package br.com.alura.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Movimentacao;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
+
+public class TesteMediaDiaDeMovimentacaoComCriteria {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+		EntityManager em = emf.createEntityManager();
+		
+		MovimentacaoDao dao = new MovimentacaoDao(em);
+		List<Movimentacao> movimentacaoFiltradaPorData = dao.getMovimentacaoFiltradaPorData(12, 01, 2017);
+		
+		for (Movimentacao movimentacao : movimentacaoFiltradaPorData) {
+			System.out.println("-----------------------------");
+			System.out.println("Descrição: " + movimentacao.getDescricao());
+			System.out.println("Valor: " + movimentacao.getValor());
+		}
+
+	}
+
+}
